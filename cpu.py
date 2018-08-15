e3f5fd70783a5d0e15b1f18a0cf6a054c87db4ed
Fabrizzio RIvera <frivera@nevicad.com>
2018-08-15 14:55:02 -0600

    Automated Test Mode

--- cpu.py
+++ cpu.py
@@ -1,2 +1,12 @@
-person = input('Enter your name: ')
-print('Hello', person)
+import sys
+
+if sys.argv[1] == "test":
+    TestMode()
+
+def TestMode():
+    print("Test mode!")
+    ## Add automated test for travis
+
+def Hello():    
+    person = input('Enter your name: ')
+    print('Hello', person)
