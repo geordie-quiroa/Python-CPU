a2d64954474bece9a74ec2bdc030e95d8ea649fc
Fabrizzio RIvera <frivera@nevicad.com>
2018-08-15 16:19:41 -0600

    Instrucition Parser

--- controlUnit.py
+++ controlUnit.py
@@ -1,6 +1,30 @@
-def Parser(machineCode):    
-    return ('Hello ' + machineCode)
+def InstructionParser(machineCode):
+    # Python Dictionary
+    ## Si "machineCode" es alguno de los elementos de la derecha entonces retornar el elemento de la izquierda.
+    ## Es similar a un switch de otros lenguajes.
+    return{
+        '0000': 'OUTPUT',
+        '0001': 'LD_A',
+        '0010': 'LD_B',
+        '0011': 'AND',
+        '0100': 'ILD_A',
+        '0101': 'STR_A',
+        '0110': 'STR_B',
+        '0111': 'OR',
+        '1000': 'ILD_B',
+        '1001': 'ADD',
+        '1010': 'SUB',
+        '1011': 'JMP',
+        '1100': 'JMP_N',
+        '1101': 'ROR?',
+        '1110': 'ROL?',
+        '1111': 'HLT'
+    }.get(machineCode, machineCode)
 
 
-if __name__ == '__main__':
-    print(Parser(input("Code: ")))
+if __name__ == '__main__': 
+    ## Si controlUnit.py está corriendo como dependencia, entonces no correr las siguientes lineas.
+    ## Cada funcion debe estar "autocontenida" para poder hacer pruebas autmatizadas.
+
+
+    print(InstructionParser(input("Code: ")))
--- test_cpu.py
+++ test_cpu.py
@@ -1,11 +1,20 @@
 import controlUnit
 import unittest
 
+##Clase de pruebas automatizadas
 class TestCPU(unittest.TestCase):
 
+    #Definicion para comprobar que la funcion Parser de ControlUnit esté funcionando.
     def test_parser(self):
-        result = controlUnit.Parser('0101')
-        self.assertEqual(result, 'Hello 0101')
+        #Listado de respuestas esperadas al parser
+        instructions = [['0000', 'OUTPUT'],['0001', 'LD_A'],['0010', 'LD_B'],['0011', 'AND'],['0100', 'ILD_A'],['0101', 'STR_A'],['0110', 'STR_B'],['0111', 'OR'],['1000', 'ILD_B'],['1001', 'ADD'],['1010', 'SUB'],['1011', 'JMP'],['1100', 'JMP_N'],['1101', 'ROR?'],['1110', 'ROL?'],['1111', 'HLT']]
+        #La variable result tiene la respuesta de la funcion Parser de Control Unit.
+        #La idea de esta prueba es comprobar que parser está respondiendo los valores esperados.
+        for inst in instructions:
+            result = controlUnit.InstructionParser(inst[0])
+            self.assertEqual(result, inst[1])
 
 if __name__ == '__main__':
+    #Si test_cpu.py está corriendo como dependencia entonces no correr las siguientes lineas:
+
     unittest.main()
