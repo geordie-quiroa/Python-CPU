4364221d6111a23cd0af37bb3acba1f1ee08c8e6
geordie <gqmain@protonmail.com>
2018-08-18 17:41:52 -0600

    cree la clase registros, atributos y metodos

--- memory.py
+++ memory.py
@@ -1,7 +1,11 @@
 class memory:
     class data:
         _readMe = open("test.code", 'r').read()
-        _data = _readMe.split('\n')  # asigna a cada linea del archivo, una posicion en el arreglo privado _data
+        _predata = _readMe.split('\n')  # asigna a cada linea del archivo, una posicion en el arreglo privado _data
+        _data = []
+        for byte in _predata:
+            byte = int(byte)
+            _data.append(byte)
         n = len(_data)
         def __init__(self):
                 self.instructions = memory.data._data #crea el atributo de instrucciones al objeto data para luego cargarlo a ram
@@ -9,7 +13,7 @@ class memory:
     class ram(data): #inherita clase data
         def __init__(self):
             _addresses = []
-            self.data = memory.data().instructions #convierto el atributo instrucciones de la clase data en un atributo arreglo de ram para poder indexar los valores
+            self.data = memory.data().instructions #cargo el atributo instrucciones de la clase data a un atributo arreglo de ram para poder indexar los valores
             self.totalData = memory.ram.n
             for address in range (0,self.totalData,1):
                 _addresses.append(address)
@@ -19,10 +23,28 @@ class memory:
         def __init__(self, PC=0): #en PC pasas el atributo que tiene el PC de tu CU, PC.Counter o algo asi
             self.instruction2exec = PC
             self.nextInstruction2exec = PC +1
-    class addressBus(MAR): #inherita atributos privados de MAR 
+    class addressBus(MAR): #inherita MAR, con el fin de retornar solo la direccion de ram para esa instruccion 
         def ramDir(self): #va a retornar la direccion de memoria (que contiene la data) que solcito el PC. Este valor lo va a usar la funcion de ram que devuelve la data en esa dirRam al CU
-            self.dir = self.instruction2exec #utiliza el atributo privado (.instruction2exec) de MAR para retornar solo la dir de memoria de la data para ejecutar la instruccion del PC
+            self.dir = self.instruction2exec #utiliza el atributo (.instruction2exec) de MAR para retornar solo la dir de memoria de la data para ejecutar la instruccion del PC
             return self.dir
+    
+    class registers:
+        inUse = 0
+        def __init__(self, n=0):
+            if n == 0:
+                self.storedValue = n
+            else:
+                self.storedValue = n
+                memory.registers.inUse += 1
+        def clearRegistry(self):
+            self.storedValue = 0
+            memory.registers.inUse -= 1
+
+        def withValue(self):
+            if self.storedValue >0:
+                return 1
+            else:
+                return 0
 
 if __name__ == '__main__':
 
@@ -37,4 +59,17 @@ if __name__ == '__main__':
         MAR = memory.MAR(i) # i es el program counter PC
         print(MAR.instruction2exec, " - %i" %MAR.nextInstruction2exec)
         print("Ir a buscar esta direccion en memoria> %i" %memory.addressBus.ramDir(MAR)) #MAR es el objeto definido dentro de este for y retorna la direccion de ram para obtener la data.
-    
+    print(MAR.instruction2exec)
+    #addressBus = memory.addressBus
+    direccionRam = memory.addressBus.ramDir(MAR)
+    print("Buscar en RAM la direccion %i" % direccionRam)
+    regA = memory.registers(direccionRam)
+    regC = memory.registers(direccionRam)
+    regB = memory.registers()
+    print("Valor del Registro A  %i" %regA.storedValue)
+    print(regB.storedValue)
+    print("Registros en uso>> %i" %memory.registers.inUse)
+    regC.clearRegistry()
+    print("Registros en uso>> %i" % memory.registers.inUse)
+    print(regC.storedValue)
+    print(regA.withValue())
