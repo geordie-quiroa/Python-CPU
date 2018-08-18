73334268bd651d647805e027308523705d96a673
geordie <gqmain@protonmail.com>
2018-08-18 12:45:11 -0600

    limpie el codigo y agregue algunos comentarios

--- memory.py
+++ memory.py
@@ -1,56 +1,37 @@
 class memory:
     class data:
-        #_opcodes = [] # variables privadas, por convencion se usa el guion bajo para este tipo de variables.
-        #_data4bits = []
-        _readMe = open("data.code", 'r').read()
-        #print(_readMe)
-        _data = _readMe.split('\n')  # crea un array
-        #_data = [] #cambie _parseData por _data
-        #print(_splitMe)
+        _readMe = open("test.code", 'r').read()
+        _data = _readMe.split('\n')  # asigna a cada linea del archivo, una posicion en el arreglo privado _data
         n = len(_data)
         def __init__(self):
-                self.instructions = memory.data._data
-        #for byte in _parseData:  # inhabilite esta funcion porque ahora _parseData es _data
-            #print(byte[4:8])
-            #_data.append(byte)
-            #_opcodes.append(byte[0:4])
-            #_data4bits.append(byte[4:8])
+                self.instructions = memory.data._data #crea el atributo de instrucciones al objeto data para luego cargarlo a ram
 
-    class ram(data): #inherits class data
+    class ram(data): #inherita clase data
         def __init__(self):
             address = []
-            #self.opcodes = memory.data._opcodes
-            #self.data = memory.data._data4bits
             self.data = memory.data._data
             self.totalData = memory.ram.n
             for i in range (0,self.totalData,1):
                 address.append(i)
             self.address = address
-    class MAR:
+    class MAR: #MAR recibe el PC del CU para ir a buscar esa data al address en ram que envio el PC, tambien almacena la siguiente linea a ejecutar para que lo use el CU
         def __init__(self, PC=0):
             self.instruction2exec = PC
             self.nextInstruction2exec = PC +1
-
 
     class addressBus:
         def __init__(self):
             self.address = "hello"
 if __name__ == '__main__':
-    #ram=memory.ram(memory.ram)
-    #memory.ram(memory.ram._splitMe)
 
     instrucciones = memory.data()
     print(instrucciones.instructions)
     ram = memory.ram()
-    #ram.opcodes
+    
     for n in range (0, memory.data.n, 1):
         print(ram.address[n], ram.data[n], ram.totalData, ram._data, ram.address)
-    #print(ram.opcode, ram.memory_address)
-    #for byte in memory.ram._splitMe:
-        #print(byte)
-    #    print(byte[0:4], byte[0:4])
     MAR = memory.MAR()
-    for i in range(0, 5, 1):
+    for i in range(0, memory.data.n, 1):
         MAR.instruction2exec = i
         MAR.nextInstruction2exec = i+1
         print(MAR.instruction2exec, " - %i" %MAR.nextInstruction2exec)
--- test_memory.py
+++ test_memory.py
@@ -7,23 +7,26 @@ class TestCPU(unittest.TestCase):
 
     def test_reader(self):
         cu = controlUnit.ControlUnit()
-        result = cu.FileReader("test.code")
+        cu.FileReader("test.code")
         #self.assertEqual(result, "00001111")
 
     def test_separator(self):
         cu = controlUnit.ControlUnit()
-        result = cu.InstructionsSeparator("0000111111110000")
+        cu.InstructionsSeparator("0000111111110000")
+        #result = cu.InstructionsSeparator("0000111111110000")
         #self.assertEqual(result, [["0000","1111"], ["1111", "0000"]])
     
     def test_fetcher(self):
         cu = controlUnit.ControlUnit()
-        result = cu.InstructionFetcher([["0000","1111"], ["1111", "0000"]], "0001")
+        cu.InstructionFetcher([["0000","1111"], ["1111", "0000"]], "0001")
+        #result = cu.InstructionFetcher([["0000","1111"], ["1111", "0000"]], "0001")
         #self.assertEqual(result, ["1111", "0000"])
 class TestMemory(unittest.TestCase):
 
     def test_data_load(self):
         m = memory.memory()
-        result = m.data()
+        m.data()
+        #result = m.data()
 
 if __name__ == '__main__':
     #Si test_cpu.py está corriendo como dependencia entonces no correr las siguientes lineas:
