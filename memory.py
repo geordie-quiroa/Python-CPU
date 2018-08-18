9052c497c1c614ea5758928924c5e53e52b5cbdd
geordie <gqmain@protonmail.com>
2018-08-17 21:18:53 -0600

    codigo comentado

--- memory.py
+++ memory.py
@@ -1,20 +1,24 @@
 class memory:
     class data:
-        opcodes = []
-        memory_address = []
-        _readMe = open("data.code", 'r').read()
+        _opcodes = [] # variables privadas, por convencion se usa el guion bajo para este tipo de variables.
+        _memory_address = []
+        _readMe = open("test.code", 'r').read()
         #print(_readMe)
-        _splitMe = _readMe.split('\n')  # crea un array
+        _splitMe = _readMe.split('\n')  # crea un array para cada linea del .code
         #print(_splitMe)
-
+        n = len(_splitMe)
         def __init__(self, _splitMe):
                 self.instructions = _splitMe
         for byte in _splitMe:
             #print(byte[4:8])
-            opcodes.append(byte[0:4])
-            memory_address.append(byte[4:10])
+            _opcodes.append(byte[0:4])
+            _memory_address.append(byte[4:10])
 
-    class ram(data):
+    class ram(data): #inherits class data
         def __init__(self):
-            self.opcodes = memory.ram.opcodes
-            self.memory_address = memory.ram.memory_address
+            self.opcodes = memory.data._opcodes
+            self.memory_address = memory.data._memory_address
+
+#ram = memory.ram()
+#for byte in range (0, memory.data.n, 1):
+    #return (ram.opcodes[0:4], ram.memory_address[4:8])
