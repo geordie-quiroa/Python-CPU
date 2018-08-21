3866860eb276585824f9843186cb381b7a81b322
geordie <gqmain@protonmail.com>
2018-08-20 23:03:58 -0600

    probe todos los modulos, y los uni en circuitoIntegrado.py

--- circuitoIntegrado.py
+++ circuitoIntegrado.py
@@ -1,2 +1,24 @@
+from memoria import ram
+from registros import registers
+from controlUnit import CU
+from MemoryAddressRegistry import MAR
+
+programa = CU.programa() #ya construi el objeto con las instrucciones de programa
+PC = CU.programCounter() # ya construi el objeto PC
+RAM = ram() # ya construi el objeto ram con sus respectivos datos 
+PC.update() #si aumenta el PC.value en uno
+CIR = CU.InstructionRegister.currentInstructionRegister(programa.instrucciones[PC.value]) #ya tengo el CurrentInstructionRegister con el byte a ejecutar en string
+MAR = MAR(PC.value) #almacena el PC.value que puede cambiar durante la ejecucion sin alterar el atributo del PC como tal.
+dataBus = RAM.dataBus(MAR.addressBus)
+print(RAM.data)
+print(RAM.dataBus(5))
+print(MAR.addressBus)
+print(dataBus)
+PC.update()
+MAR.addressBus = 6 #cambio 
+print(MAR.addressBus)
+print(dataBus)
+print(CIR.current)
+print(CIR.FourBitsAddressInfo)
 def comentario():
     print('aqui voy a integrar todos los modulos creados.')
--- controlUnit.py
+++ controlUnit.py
@@ -22,119 +22,67 @@ class CU:
         def update(self):
             self.value += 1
             
-    class currentInstructionRegister:
-        def __init__(self, instructionAtPC): #tiene que recibir el string del programa.code at PC. Sin procesar ni nada, porque adentro lo procesa.
-            self.current = instructionAtPC
-            _index = instructionAtPC.find(' ')
-            _instructionLength = len(instructionAtPC)
-            if _index >0:
-                self.opcode = instructionAtPC[0:_index]
-                self.FourBitsAddressInfo = instructionAtPC[_index:_instructionLength]
-            else:
-                self.opcode = instructionAtPC # ver cual es la direccion de halt aqui va a ir
-                self.FourBitsAddressInfo = instructionAtPC #ver cual es la direccion de HALT, aqui va a ir
-        def decode(self):
-            #_data2evaluate = CU.InstructionRegister()
-            #_data2evaluate.evaluate(self.opcode)
-            opcode = self.opcode
-            if opcode == 'OUTPUT' or opcode == '0000':
-                return('Fuciono output')
-            if opcode == 'LD_A' or opcode == '0001':
-                return('Funciono LD_A')
-                ## DO OUTPUT
-            elif opcode == 'LD_B' or opcode == '0010':
-                return('Funciono LD_B')
-                ## DO LD_A
-            elif opcode == 'AND' or opcode == '0011':
-                return ('Funciono AND')
-                ## DO LD_B
-            elif opcode == 'ILD_A' or opcode == '0100':
-                return ('Funciono ILD_A')
-                ## DO AND
-            elif opcode == 'ILD_B' or opcode == '0010':
-                return ('Funciono ILD_B')
-            elif opcode == 'STR_A' or opcode == '0101':
-                return ('Funciono STR_A')
-                ## DO ILD_A
-            elif opcode == 'STR_B' or opcode == '0110':
-                return ('Funciono STR_B')
-                ## DO STR_A
-            elif opcode == 'STR_C' or opcode == '0111':
-                return ('Funciono STR_C')
-                ## DO STR_B
-            elif opcode == 'ADD' or opcode == '1001':
-                return ('Funcionon ADD')
-                ## DO ILD_B
-            elif opcode == 'SUB' or opcode == '1010':
-                return ('Funciono SUB')
-                ## DO ADD
-            elif opcode == 'JMP' or opcode == '1011':
-                return ('Funciono JMP')
-                ## DO SUB
-            elif opcode == 'JMP_N' or opcode == '1100':
-                return ('Funciono JMP_N')
-                ## DO SUB
-            elif opcode == 'ROR?' or opcode == '1101':
-                ## DO JMP
-                return ("Funciono ROR?")
-            elif opcode == 'ROL?' or opcode == '1110':
-                return ('Funciono ROL?')
-            elif opcode == 'HLT' or opcode == '1111':
-                return ('Funciono HALT')
-    
     class InstructionRegister:
-        def evaluate(self, opcodeSent):
-            opcode = opcodeSent
-            if opcode == 'OUTPUT' or opcode == '0000':
-                return(opcode + '1')
-            if opcode == 'LD_A' or opcode == '0001':
-                return(opcode)
-                ## DO OUTPUT
-            elif opcode == 'LD_B' or opcode == '0010':
-                print(opcode)
-                ## DO LD_A
-            elif opcode == 'AND' or opcode == '0011':
-                print(opcode)
-                ## DO LD_B
-            elif opcode == 'ILD_A' or opcode == '0100':
-                print(opcode)
-                ## DO AND
-            elif opcode == 'ILD_B' or opcode == '0010':
-                print(opcode)
-            elif opcode == 'STR_A' or opcode == '0101':
-                print(opcode)
-                ## DO ILD_A
-            elif opcode == 'STR_B' or opcode == '0110':
-                print(opcode)
-                ## DO STR_A
-            elif opcode == 'STR_C' or opcode == '0111':
-                print(opcode)
-                ## DO STR_B
-            elif opcode == 'ADD' or opcode == '1001':
-                print(opcode)
-                ## DO ILD_B
-            elif opcode == 'SUB' or opcode == '1010':
-                print(opcode)
-                ## DO ADD
-            elif opcode == 'JMP' or opcode == '1011':
-                print(opcode)
-                ## DO SUB
-            elif opcode == 'JMP_N' or opcode == '1100':
-                print(opcode)
-                ## DO SUB
-            elif opcode == 'ROR?' or opcode == '1101':
-                ## DO JMP
-                print("TEST")
-            elif opcode == 'ROL?' or opcode == '1110':
-                print(opcode)
-            elif opcode == 'HLT' or opcode == '1111':
-                print(opcode)
-
-    def InstructionDecoder(self, opcode, data):
-        opcode = int(opcode, 2)
-        
-    def halt(self):
-        exit()
+        class currentInstructionRegister:
+            def __init__(self, instructionAtPC): #tiene que recibir el string del programa.code at PC. Sin procesar ni nada, porque adentro lo procesa.
+                self.current = instructionAtPC
+                _index = instructionAtPC.find(' ')
+                _instructionLength = len(instructionAtPC)
+                if _index >0:
+                    self.opcode = instructionAtPC[0:_index]
+                    self.FourBitsAddressInfo = instructionAtPC[_index+1:_instructionLength]
+                else:
+                    self.opcode = instructionAtPC # ver cual es la direccion de halt aqui va a ir
+                    self.FourBitsAddressInfo = instructionAtPC #ver cual es la direccion de HALT, aqui va a ir
+            def decode(self):
+                opcode = self.opcode
+                if opcode == 'OUTPUT' or opcode == '0000':
+                    return('Fuciono output')
+                if opcode == 'LD_A' or opcode == '0001':
+                    return('Funciono LD_A')
+                    ## DO OUTPUT
+                elif opcode == 'LD_B' or opcode == '0010':
+                    return('Funciono LD_B')
+                    ## DO LD_A
+                elif opcode == 'AND' or opcode == '0011':
+                    return ('Funciono AND')
+                    ## DO LD_B
+                elif opcode == 'ILD_A' or opcode == '0100':
+                    return ('Funciono ILD_A')
+                    ## DO AND
+                elif opcode == 'ILD_B' or opcode == '0010':
+                    return ('Funciono ILD_B')
+                elif opcode == 'STR_A' or opcode == '0101':
+                    return ('Funciono STR_A')
+                    ## DO ILD_A
+                elif opcode == 'STR_B' or opcode == '0110':
+                    return ('Funciono STR_B')
+                    ## DO STR_A
+                elif opcode == 'STR_C' or opcode == '0111':
+                    return ('Funciono STR_C')
+                    ## DO STR_B
+                elif opcode == 'ADD' or opcode == '1001':
+                    return ('Funcionon ADD')
+                    ## DO ILD_B
+                elif opcode == 'SUB' or opcode == '1010':
+                    return ('Funciono SUB')
+                    ## DO ADD
+                elif opcode == 'JMP' or opcode == '1011':
+                    return ('Funciono JMP')
+                    ## DO SUB
+                elif opcode == 'JMP_N' or opcode == '1100':
+                    return ('Funciono JMP_N')
+                    ## DO SUB
+                elif opcode == 'ROR?' or opcode == '1101':
+                    ## DO JMP
+                    return ("Funciono ROR?")
+                elif opcode == 'ROL?' or opcode == '1110':
+                    return ('Funciono ROL?')
+                elif opcode == 'HLT' or opcode == '1111':
+                    return ('Funciono HALT')
+        class operations:
+            def ADD(self):
+                print("En construccion")
 
 
 if __name__ == '__main__': 
@@ -145,14 +93,14 @@ if __name__ == '__main__':
         
         for i in range(0, programa.n-1,1):
             print(PC.value)
-            CIR = CU.currentInstructionRegister(ProgInstrucciones[PC.value])
+            CIR = CU.InstructionRegister.currentInstructionRegister(ProgInstrucciones[PC.value])
             print(CIR.current, "opcode> %s" %CIR.opcode, "address Info> %s" %CIR.FourBitsAddressInfo)
             print(CIR.decode())
             PC.update()
             print(programa.instrucciones[i])
         i = 2
         index = ProgInstrucciones[i].find(' ')
-        CIR = CU.currentInstructionRegister(ProgInstrucciones[PC.value])
+        CIR = CU.InstructionRegister.currentInstructionRegister(ProgInstrucciones[PC.value])
         print(ProgInstrucciones[i][0:index])
         print(ProgInstrucciones)
         print(CIR.decode())
@@ -164,7 +112,7 @@ if __name__ == '__main__':
         for i in range (0, programa.n-1, 1):
             print(progInstructions[i])
             print(PC.value)
-            CIR = CU.currentInstructionRegister(progInstructions[PC.value])
+            CIR = CU.InstructionRegister.currentInstructionRegister(progInstructions[PC.value])
             print(CIR.current)
             print(CIR.decode())
             PC.update()
