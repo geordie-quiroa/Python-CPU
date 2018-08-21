89c761c2f7e51ff7ed142a698c9c7af77ab403eb
Fabrizzio RIvera <frivera@nevicad.com>
2018-08-20 20:04:36 -0600

    memory Load automated test

--- test.py
+++ test.py
@@ -28,7 +28,8 @@ class TestMemory(unittest.TestCase):
 
     def test_data_load(self):
         _m = memory.memory()
-        _m.data()
+        _result = _m.data().instructions
+        self.assertEqual(_result, ['00001111','11110000','10100000','11100111'])
 
 if __name__ == '__main__':
     #Si test.py está corriendo como dependencia entonces no correr las siguientes lineas:
